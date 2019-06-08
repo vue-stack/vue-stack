17de65455f99c6f5da6b9e2236d0856fb9d28722
devam.stimulus <devam@stimulusconsultancy.com>
2019-06-08 19:55:23 +0530

    Deployed for GH-PAGES

--- src/services/Api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export default() => {
-  return axios.create({
-    baseURL: `https://api.stackexchange.com/2.2/`
-  })
-}
--- src/services/QuestionService.js
+++
@@ -1,7 +0,0 @@
-import Api from '@/services/Api'
-
-export default {
-  fetchFeaturedQuestions () {
-    return Api().get('questions/featured?order=desc&sort=activity&site=stackoverflow');
-  }
-}
